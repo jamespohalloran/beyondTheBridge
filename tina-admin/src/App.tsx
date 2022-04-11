a779f171675d46ef09bbef4c082f231454d52b31
James O'Halloran <james.p.ohalloran@gmail.com>
2022-04-11 11:21:38 -0300

    admin dir

--- tina-admin/src/App.tsx
+++ tina-admin/src/App.tsx
@@ -4,8 +4,8 @@ import { TinaEditProvider, setEditing } from "tinacms/dist/edit-state";
 function App() {
   setEditing(true);
 
-  const branch = "main";
-  const clientId = "";
+  const branch = "tina-setup";
+  const clientId = "3ae1db1c-4e84-4847-bee4-c633f5c9bfae";
   const apiURL =
     process.env.NODE_ENV == "development"
       ? "http://localhost:4001/graphql"
