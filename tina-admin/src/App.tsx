82a301d4d974edb4d31da27075a1865ce8b31c9e
James O'Halloran <james.p.ohalloran@gmail.com>
2022-05-02 16:55:45 -0300

    add tina

---
+++ tina-admin/src/App.tsx
@@ -0,0 +1,27 @@
+import TinaCMS, { TinaAdmin } from "tinacms";
+import { TinaEditProvider, setEditing } from "tinacms/dist/edit-state";
+
+function App() {
+  setEditing(true);
+
+  const branch = "tina-test";
+  const clientId = "3ae1db1c-4e84-4847-bee4-c633f5c9bfae";
+  const apiURL =
+    process.env.NODE_ENV == "development"
+      ? "http://localhost:4001/graphql"
+      : `https://content.tinajs.io/content/${clientId}/github/${branch}`;
+
+  return (
+    <TinaEditProvider
+      editMode={
+        <TinaCMS apiURL={apiURL}>
+          <TinaAdmin />
+        </TinaCMS>
+      }
+    >
+      <div>Need to be in edit mode!</div>
+    </TinaEditProvider>
+  );
+}
+
+export default App;
